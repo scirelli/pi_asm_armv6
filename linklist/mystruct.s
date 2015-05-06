b0a8a0cb0ce73b471a294e6694babf2b8f4d5a88
Steve Cirelli <scirelli+pib_1@gmail.com>
2015-05-06 17:21:49 +0000

    stuff

--- linklist/mystruct.s
+++ linklist/mystruct.s
@@ -8,3 +8,9 @@ node:
 .FUNC main
 
 main:
+    STMFD sp!, {r4-r12,LR}      @ Store registerst that need to be preserved including the link reg.
+
+
+exit:
+    LDMFD sp!, {r4-r12,LR}      @ Restore the registers and link reg.
+    MOV PC, LR
