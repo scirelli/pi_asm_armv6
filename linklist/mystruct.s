11a0954fedf35365b404eaeb35adfb5365626132
Steve Cirelli <scirelli+pib_1@gmail.com>
2015-05-15 01:17:24 +0000

    printList function

--- linklist/mystruct.s
+++ linklist/mystruct.s
@@ -8,19 +8,23 @@ node:
     .WORD 0   @ p_next
 p_head: .WORD 0
 p_tail: .WORD 0
-ubyte_cnt=10
+cnt=10
+null=0
 s_fmt: .asciz "%d\n"
 .TEXT
 .ALIGN 2
 .GLOBAL main
 .FUNC main
 
+@┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+@│ main()                                          │
+@└─────────────────────────────────────────────────┘  
 main:
     STMFD sp!, {r4-r12,LR}      @ Store registerst that need to be preserved including the link reg.
     
-@┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
-@│ First use local variable to create a node       │
-@└─────────────────────────────────────────────────┘  
+    @┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+    @│ First use local variable to create a node       │
+    @└─────────────────────────────────────────────────┘  
     MOV r0, #1
     LDR r1, =node
     STR r0, [r1]
@@ -28,9 +32,9 @@ main:
     MOV r0, #2
     STR r0, [r1,#4]
     
-@┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
-@│ Then try and create one on the heap             │
-@└─────────────────────────────────────────────────┘  
+    @┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+    @│ Then try and create one on the heap             │
+    @└─────────────────────────────────────────────────┘  
     MOV r0, #4                  @ size of block, 4 bytes
     BL  malloc                  @ create space for a new node. r0 will hold the pointer to the new block
 
@@ -39,9 +43,9 @@ main:
     MOV r2, #5
     STR r2, [r0] 
 
-@┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
-@│ Then a second one and link them                 │
-@└─────────────────────────────────────────────────┘  
+    @┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+    @│ Then a second one and link them                 │
+    @└─────────────────────────────────────────────────┘  
     MOV r0, #4                  @ size of block, 4 bytes
     BL  malloc                  @ create space for a new node
     
@@ -50,14 +54,14 @@ main:
     LDR r1, [r4]                @ get the node at p_head
     STR r0, [r1, #4]            @ set p_head->p_next to the address of the new node
 
-@┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
-@│ Now try to create a long chain in a loop        │
-@└─────────────────────────────────────────────────┘  
+    @┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
+    @│ Now try to create a long chain in a loop        │
+    @└─────────────────────────────────────────────────┘  
     LDR r4, =p_tail
     STR r0, [r4]                @ Track the last node in p_tail 
-    MOV r5, #ubyte_cnt          @ loop counter
+    MOV r5, #cnt                @ loop counter
 
-fill_loop:
+    fill_loop:
     MOV r0, #4                  @ size of block, 4 bytes
     BL  malloc                  @ create space for a new node
     STR r5, [r0]                @ store the loop index as the data
@@ -70,31 +74,37 @@ fill_loop:
     SUBS r5, r5, #1             @ Decrement the index by 1
     BNE fill_loop               @ Continue looping if we're not at 0
     
+    LDR r0, =p_head            @ get the head pointer.
+
+    BL printList
+BAL exit                       @ return
+
 @┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
-@│ Loop through the list and print it.             │
+@│ printList()                                     │
+@│ desc: Loop through the list and print it.       │
+@│ param: pointer to the head node.                │
+@│ return: nothing                                 │
 @└─────────────────────────────────────────────────┘  
-    LDR r1, =p_head            @ get the head pointer.
+printList:
+    STMFD sp!, {lr}            @ Store registerst that need to be preserved including the link reg.
+    MOV r1, r0                 @ prepare to call printf by putting the head node in r1
     LDR r1, [r1]               @ get the first node
-print_loop:
-    STMFD sp!, {r1}
-    LDR r1, [r1]
-    BL _print
-    LDMFD sp!, {r1}
-    LDR r1, [r1,#4]
-    CMP r1, #0
+    print_loop:
+        LDR r0, =s_fmt         @ keep the format string in r0
+        STMFD sp!, {r1}
+        LDR r1, [r1]           @ get the value from the node
+        BL printf              @ print the value
+        LDMFD sp!, {r1}
+        LDR r1, [r1,#4]        @ load the address of the next node into r1
+        CMP r1, #null          @ see if next node is null
     BNE print_loop
-    BAL exit
+    LDMFD sp!, {lr}            @ Restore the registers and link reg.
+@ ─────────────────────────────────────────────────
+MOV pc, lr                     @ return
 
 @┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
-@│ Print a number                                  │
+@│ Exit                                            │
 @└─────────────────────────────────────────────────┘  
-_print:
-    LDR r0, =s_fmt
-    STMFD sp!, {lr}
-    BL printf
-    LDMFD sp!, {lr} 
-    MOV pc, lr
-
 exit:
     MOV r0, #0
     LDMFD sp!, {r4-r12,LR}      @ Restore the registers and link reg.
